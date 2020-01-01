8be20b33fc368d9778cac1d115bf7dca4a6df4dd
MarmoriPelaa <marcus.erkkila.me@gmail.com>
2020-01-02 00:58:59 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ if sys.version_info[0] is 3:
         def do_POST(self):
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(str.encode("server|127.0.0.1\nport|17091\ntype|1\n#maint|Mainetrance message (Not used for now) -- Growtopia Noobs\n\nbeta_server|beta.growtopiagame.com\nbeta_port|27003\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001"))
+            self.wfile.write(str.encode("server|127.0.0.1\nport|17125\ntype|1\n#maint|Mainetrance message (Not used for now) -- Growtopia Noobs\n\nbeta_server|beta.growtopiagame.com\nbeta_port|27003\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001"))
     server_address = ('', 80)
     httpd = http.server.HTTPServer(server_address, ServerHandler)
     print("HTTP server is running! Don't close this or GT will be unable to connect!")
